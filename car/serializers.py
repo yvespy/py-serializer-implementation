70a2a7de70049a729c6a9e6b23272722536622ed
yevpy <eugene2piskun@gmail.com>
2025-04-18 23:42:58 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -7,4 +7,7 @@ class CarSerializer(serializers.Serializer):
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_blank=True, required=False)
+    problem_description = serializers.CharField(
+        allow_blank=True,
+        required=False
+    )
